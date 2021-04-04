52e13a9f044e26e86e94bd30c8761874984e30cf
Vladislav Sheyko <45404995+liquiz0v@users.noreply.github.com>
2021-04-04 19:26:03 +0300

    Feature/files endpoints and job controls (#2)
    * Implemented "Get Files" proxy method
    
    * Added job control endpoints
    
    Co-authored-by: Vladislav Sheyko <vladislav.sheyko@hotmail.com>

--- DistancePrinterControl.API/DistancePrinterControl.API/Controllers/PrinterController.cs
+++ DistancePrinterControl.API/DistancePrinterControl.API/Controllers/PrinterController.cs
@@ -1,19 +1,23 @@
 ﻿using System;
 using System.Collections.Generic;
 using System.Data;
+using System.IO;
 using System.Linq;
 using System.Net.Http;
 using System.Net.Http.Headers;
+using System.Text;
 using System.Threading.Tasks;
 using Dapper;
 using DistancePrinterControl.API.Helpers;
 using DistancePrinterControl.Database.Logic.ReadServices.Interfaces;
 using DistancePrinterControl.Database.Models;
+using DistancePrinterControl.Logic.DTO;
 using Microsoft.AspNetCore.Mvc;
 using Microsoft.Data.SqlClient;
 using Microsoft.EntityFrameworkCore.Storage.ValueConversion;
 using Microsoft.Extensions.Logging;
 using Microsoft.IdentityModel.Tokens;
+using Newtonsoft.Json;
 
 namespace DistancePrinterControl.API.Controllers
 {
@@ -63,6 +67,84 @@ namespace DistancePrinterControl.API.Controllers
                 Right = "/right",
                 PrinterId = $@"{printerId}"
             };
+        }
+        
+        [HttpPost("{printerId}/files")]
+        public async Task<object> UploadFile([FromForm] UploadFileDTO uploadData, int printerId) 
+            // TODO: Fix this. Learn about multipart forms, and try to implement proxy here.
+        {
+            var printer = _readService.GetPrinter(printerId);
+
+            using (var client = new HttpClient())
+            {
+                client.DefaultRequestHeaders.Authorization
+                    = new AuthenticationHeaderValue("Bearer", $@"{_printerCreds}");
+
+                var multipartContent = new MultipartFormDataContent();
+
+                if (uploadData.File.Length > 0)
+                {
+                    using (var ms = new MemoryStream())
+                    {
+                        uploadData.File.CopyTo(ms);
+                        var fileBytes = ms.ToArray();
+                        var byteString = Convert.ToBase64String(fileBytes);
+                        multipartContent.Add(new StringContent(byteString), uploadData.Name.ToString());
+                    }
+                }
+            
+
+                var response = await client.PostAsync($"{printer.PrinterUrl}/api/files/local", multipartContent);
+                var responseBody = await response.Content.ReadAsStringAsync();
+                return responseBody;
+            }
+        }
+        
+        [HttpGet("{printerId}/files")]
+        public async Task<object> GetFiles(int printerId) // Seems to be OK, but think about deserialization on client.
+        {
+            var printer = _readService.GetPrinter(printerId);
+            
+            var client = new HttpClient();
+            client.DefaultRequestHeaders.Authorization
+                = new AuthenticationHeaderValue("Bearer", $@"{_printerCreds}");
+
+            var response = await client.GetAsync($"{printer.PrinterUrl}/api/files/local");
+            var responseBody = await response.Content.ReadAsStringAsync();
+            return responseBody;
+        }
+
+        [HttpGet("{printerId}/job")]
+        public async Task<object> JobStatus(int printerId)
+        {
+            var printer = _readService.GetPrinter(printerId);
+            
+            var client = new HttpClient();
+            client.DefaultRequestHeaders.Authorization
+                = new AuthenticationHeaderValue("Bearer", $@"{_printerCreds}");
+
+            var response = await client.GetAsync($"{printer.PrinterUrl}/api/job");
+            var responseBody = await response.Content.ReadAsStringAsync();
+            return responseBody;
+        }
+        
+        [HttpGet("{printerId}/job/{command}")] 
+        //TODO: handle 409 CONFLICT when printer is not operational + put available commands into AvailableOptionsEnum
+        public async Task<object> JobIssueCommand(int printerId, string command)
+        {
+            var printer = _readService.GetPrinter(printerId);
+            
+            var client = new HttpClient();
+            client.DefaultRequestHeaders.Authorization
+                = new AuthenticationHeaderValue("Bearer", $@"{_printerCreds}");
+
+            var commandJsonObject = JsonConvert.SerializeObject(new {command = command});
+            
+            var content = new StringContent(commandJsonObject, Encoding.UTF8, "application/json");
+
+            var response = await client.PostAsync($"{printer.PrinterUrl}/api/job", content);
+            var responseBody = await response.Content.ReadAsStringAsync();
+            return responseBody;
         }
     }
 }
