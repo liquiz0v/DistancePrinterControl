6868fb5ac22302fef56b2453958b8101e4f1e921
Vladislav Sheyko <45404995+liquiz0v@users.noreply.github.com>
2021-04-04 19:26:03 +0300

    Feature/files endpoints and job controls (#2)
    * Implemented "Get Files" proxy method
    
    * Added job control endpoints
    
    Co-authored-by: Vladislav Sheyko <vladislav.sheyko@hotmail.com>

---
+++ DistancePrinterControl.API/DistancePrinterControl.Logic/DTO/GetFilesDTO.cs
@@ -0,0 +1,9 @@
+﻿namespace DistancePrinterControl.Logic.DTO
+{
+    public class GetFilesDTO
+    {
+        public string Name { get; set; }
+        public string Download { get; set; }
+        public string Resource { get; set; }
+    }
+}
