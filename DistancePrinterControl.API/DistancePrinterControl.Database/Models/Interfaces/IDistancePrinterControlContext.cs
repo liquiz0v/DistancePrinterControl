b961944631373c29442a75f5e35fe64d3e7bb933
Vladislav Sheyko <45404995+liquiz0v@users.noreply.github.com>
2021-05-23 23:08:18 +0300

    add services and repository (#5)
    * Added repository and service
    
    * moved db context and added to autofac config
    
    * Added PrinterWriteService functionality and registered in Autofac
    
    Co-authored-by: Vladislav Sheyko <vladislav.sheyko@hotmail.com>

---
+++ DistancePrinterControl.API/DistancePrinterControl.Database/Models/Interfaces/IDistancePrinterControlContext.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Threading;
+using System.Threading.Tasks;
+using Microsoft.EntityFrameworkCore;
+
+namespace DistancePrinterControl.Database.Models.Interfaces
+{
+    public interface IDistancePrinterControlContext : IDisposable
+    {
+        DbSet<Printer> Printers { get; set; }
+        
+        Task<bool> SaveEntitiesAsync(CancellationToken cancellationToken = default);
+    }
+}
---
+++ DistancePrinterControl.API/DistancePrinterControl.Database/Repository/PrinterRepository.cs
@@ -0,0 +1,55 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using DistancePrinterControl.Database.Models;
+using DistancePrinterControl.Database.Models.Interfaces;
+using DistancePrinterControl.Database.Repository.Interfaces;
+using Microsoft.EntityFrameworkCore;
+
+namespace DistancePrinterControl.Database.Repository
+{
+    public class PrinterRepository : IPrinterRepository
+    {
+        private IDistancePrinterControlContext _context;
+        
+        public PrinterRepository(IDistancePrinterControlContext context)
+        {
+            _context = context ?? throw new ArgumentNullException(nameof(context));
+        }
+        
+        public async Task<Printer> GetPrinterById(int printerId)
+        {
+            return await _context.Printers.FirstOrDefaultAsync(i => i.Id == printerId);
+        }
+
+        public async Task<bool> UpdatePrinterById(Printer printer)
+        {
+            var currentEntity = await _context.Printers.FirstOrDefaultAsync(i => i.Id == printer.Id);
+
+            currentEntity.Id = printer.Id;
+            currentEntity.PrinterUrl = printer.PrinterUrl;
+
+            _context.Printers.Update(currentEntity);
+            return await _context.SaveEntitiesAsync();
+        }
+
+        public async Task<bool> AddPrinter(Printer printer)
+        {
+            _context.Printers.Add(printer);
+            return await _context.SaveEntitiesAsync();
+        }
+
+        public async Task<bool> DeletePrinterById(int printerId)
+        {
+            var entity = await _context.Printers.FirstOrDefaultAsync(i => i.Id == printerId);
+            _context.Printers.Remove(entity);
+            return await _context.SaveEntitiesAsync();
+        }
+
+        public async Task<List<Printer>> GetPrinters()
+        {
+            return await _context.Printers.ToListAsync();
+        }
+    }
+}
